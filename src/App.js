fefb74bc9318d4426a361476419f67541beefa70
junaidMengal <junaidmengalit@gmail.com>
2022-10-25 22:25:44 +0500

    reactApp

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Counter from './counter/Counter';
+
+function App() {
+  return (
+    <div className="App">
+      
+        
+        
+      <Counter/>
+        
+      
+    </div>
+  );
+}
+
+export default App;
